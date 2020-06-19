f08050c0bdaef22b058ec6dc56ceddc9b0f6b315
bmosz <wojciech.szychta@persgroep.net>
2020-06-19 07:13:51 +0200

    Add allow all egress rule for internal communication between in internal security group

--- main.tf
+++ main.tf
@@ -196,6 +196,14 @@ resource "aws_security_group" "ecs_sg_internal" {
     self        = true
     description = "All resources using this SG have unlimited access to each other"
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   tags = merge(var.tags, { "Name" = "${module.label.id}-ecs-internal" })
 }
 locals {
