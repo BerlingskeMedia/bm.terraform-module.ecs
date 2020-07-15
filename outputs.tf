2460fe681e649ee989e666ff0c62617cea01ff8a
bmosz <wojciech.szychta@persgroep.net>
2020-07-15 10:48:18 +0200

    Remoev RDS from ecs module

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "rds_endpoint" {
-  description = "Endpoint url of created RDS"
-  value       = module.rds.rds_endpoint
-}
-
-output "environments_rds" {
-  description = "Environment variables generated due process of creating resources"
-  value       = local.rds_envs
-}
-
 output "ecs_cluster_arn" {
   value = aws_ecs_cluster.default.arn
 }
@@ -15,11 +5,6 @@ output "ecs_cluster_arn" {
 output "service_internal_security_group" {
   value       = aws_security_group.ecs_sg_internal.id
   description = "Security group used inside services"
-}
-
-output "rds_security_group" {
-  value       = module.rds.rds_sg_id
-  description = "Security group allows to connect with RDS"
 }
 
 output "alb_sg" {
@@ -30,7 +15,6 @@ output "iam_policy_document_json" {
   value = join("", data.aws_iam_policy_document.ecs_exec.*.json)
 }
 
-
 output "aws_cloudwatch_log_group_name" {
   value = aws_cloudwatch_log_group.app.name
 }
@@ -40,18 +24,6 @@ output "access_key" {
 }
 output "secret_key" {
   value = join("", data.aws_ssm_parameter.secret_key.*.value)
-}
-
-output "ecr_urls" {
-  value = module.ecr.name_to_url
-}
-
-output "rds_kms_arn" {
-  value = var.run_rds ? module.rds.kms_arn : ""
-}
-
-output "rds_kms_key_id" {
-  value = var.run_rds ? module.rds.kms_key_id : ""
 }
 
 output "ecs_ec2_role_arn" {
