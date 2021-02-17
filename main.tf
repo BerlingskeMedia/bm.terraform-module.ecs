eb0cc451c5e21f141d14dedbfcfbc223dee84975
bmosz <wojciech.szychta@persgroep.net>
2021-02-17 12:57:41 +0100

    Comment probably depricated code for testing

--- main.tf
+++ main.tf
@@ -332,9 +332,9 @@ module "drone-io" {
   attributes = compact(concat(var.attributes, ["drone"]))
 }
 
-locals {
-  repository_name = length(module.ecr.repository_name) > 0 ? element(module.ecr.repository_name, 0) : ""
-}
+# locals {
+#   repository_name = length(module.ecr.repository_name) > 0 ? element(module.ecr.repository_name, 0) : ""
+# }
 
 data "aws_iam_policy_document" "ecs_exec" {
   count = var.enabled ? 1 : 0
