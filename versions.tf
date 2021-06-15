5f41bcc04f3f8645b0b66d61a63bb9d4d57ea4c4
Maciej Gierwatowski <maciej.gierwatowski@persgroep.net>
2021-06-15 07:53:42 +0200

    template provider

--- versions.tf
+++ versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.38"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
     local = {
       source  = "hashicorp/local"
       version = ">= 1.2"
